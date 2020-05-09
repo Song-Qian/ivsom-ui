80bb34f70ee7cac16a68388aa2fe8d2c9fe25e60
SongQian <onlylove1172559463@vip.qq.com>
2020-05-09 21:03:32 +0800

    delete dist

--- dist/types/components/tabs/ivsom-tabs-panel.d.ts
+++
@@ -1,5 +0,0 @@
-import * as tsx from 'vue-tsx-support';
-export default class iVsomTabsPanel extends tsx.Component<any> {
-    constructor();
-    protected render(): JSX.Element;
-}
--- dist/types/components/tabs/ivsom-tabs.d.ts
+++
@@ -1,9 +0,0 @@
-import * as tsx from 'vue-tsx-support';
-declare type ScopedSlots = {
-    default: void;
-};
-export default class iVsomTabs extends tsx.Component<any, any, ScopedSlots> {
-    constructor();
-    protected render(): JSX.Element;
-}
-export {};
