269bd84b103870a57f2948040d1c0fb8f0d4f196
donggl163 <13476057275@163.com>
2020-06-30 17:59:34 +0800

    修改分页组件点击前后省略号显示逻辑

--- src/components/page/ivsom-pagination.tsx
+++ src/components/page/ivsom-pagination.tsx
@@ -5,188 +5,174 @@
  * Description  :   pagination组件
  */
 import * as tsx from 'vue-tsx-support'
-import { Component, Prop, Emit, Watch,} from 'vue-property-decorator'
+import { Component, Prop, Emit, Watch, } from 'vue-property-decorator'
 
 import 'vue-tsx-support/enable-check'
 
-interface Props{
-    pageNumber:Number,//当前页
-    pageSize:number[],//每页显示条数
-    totalCount:Number,//总条数
-    pageGroup:Number,//连续页码个数
-   
+interface Props {
+  currentPage: Number,//当前
+  totalCount: Number,//总条数
+  limit:Number//每页显示条数
+
 }
 interface HTMLInputEvent extends Event {
-  target: HTMLInputElement & EventTarget&InputEvent;
+  target: HTMLInputElement & EventTarget & InputEvent;
 }
 
 @Component
-export default class iVsomPagination extends tsx.Component<Props,Event>{
-    constructor(){
-        super()
-    }
-    @Prop ({default:1}) readonly pageNumber!:number
-    @Prop ({default:[10,20,30,50]}) readonly pagesize!:number[]
-    @Prop ({}) readonly totalcount!:number
-    @Prop ({default:5}) readonly pagegroup!:number
-    
-   
-    private currentPage=this.pageNumber
-    private currentSize:any=this.getCurrent
-    private jumpPageNumber=1
-    private showPrevMore=false
-    private showNextMore=false
+export default class iVsomPagination extends tsx.Component<Props, Event>{
+  constructor() {
+    super()
+  }
 
-    @Watch("currentSize")
-    currentSizeValue (newValue:any, oldValue:any) {
-      if(newValue !== oldValue){
-        if(this.currentPage >= this.totalPage){ //当前页面大于总页面数
-          this.currentPage = this.totalPage
+  @Prop({ default: 1 }) currentPage!: number
+  @Prop({ default: 1 }) limit!: number
+  @Prop({}) readonly totalcount!: number
+  @Prop({default:false}) readonly showtotal!: boolean
+  @Prop({default:false}) readonly showsize!: boolean
+  @Prop({default:false}) readonly showelevator!: boolean
+
+
+
+  private  limitNums=[5,10,15,20,25,30]
+  protected limitNum=this.limit
+  private jumpPageNumber=1
+ 
+  get totalPage(){
+    return Math.ceil(this.totalcount / this.limit)
+  }
+
+  get indexs() {
+    console.log(this.totalcount, this.limit)
+    console.log(this.totalPage)
+    var left = 1
+    var right = this.totalPage
+    var ar = []
+    if (this.totalPage >= 11) {
+      if (this.currentPage > 5 && this.currentPage < this.totalPage - 4) {
+        left = this.currentPage - 5
+        right = this.currentPage + 4
+      } else {
+        if (this.currentPage <= 5) {
+          left = 1
+          right = 10
+        } else {
+          right = this.totalPage
+          left = this.totalPage - 9
         }
-        this.$emit('jumpPage', {currentPage: this.currentPage, currentSize: this.currentSize})
       }
     }
-
-    protected created(){
-     this.currentSize=this.pagesize[0]
-     this.currentPage=this.pageNumber
+    while (left <= right) {
+      ar.push(left)
+      left++
     }
-    get showPageHelper (){
-      return this.totalcount > 0
-    }
-
-    get getCurrentPage(){
-      return this.currentPage
-    }
-
-    get getCurrent(){
-       return this.currentSize=this.pagesize[0]
-    }
-
-    get totalPage (){
-      return Math.ceil(this.totalcount /Number(this.currentSize));
-
-    }
-
-    get groupList (){
-      const groupArray = []
-      const totalPage = this.totalPage   
-      const pageGroup = this.pagegroup
-      const _offset = (pageGroup - 1) / 2
-      let current = this.currentPage
-      const offset = {
-        start: current - _offset,
-        end: current + _offset
-      }
-      if (offset.start < 1) {
-        offset.end = offset.end + (1 - offset.start)
-        offset.start = 1
-      }
-      if (offset.end > totalPage) {
-        offset.start = offset.start - (offset.end - totalPage)
-        offset.end = totalPage
-      }
-      if (offset.start < 1) offset.start = 1
-      this.showPrevMore = (offset.start > 1)
-      this.showNextMore = (offset.end < totalPage)
-      console.log(offset.end)
-      for (let i = offset.start; i <= offset.end; i++) {
-        groupArray.push(i)
-      }
-      return groupArray
-    }
-
-    protected prevPage (){
-      if (this.currentPage > 1) {
-        this.jumpPage(this.currentPage - 1)
-      }
-    }
-
-    protected  nextPage () {
-      var arr = document.getElementsByClassName("page-common")
+    if (ar[0] > 1) {
+      ar[0] = 1;
+      ar[1] = -1;
      
-      if (this.currentPage < this.totalPage) {
-        if(this.currentPage>1){
-          document.getElementsByClassName("page-item")[1].classList.remove("disabled")
+    }
+    if (ar[ar.length - 1] < this.totalPage) {
+      ar[ar.length - 1] = this.totalPage;
+      ar[ar.length - 2] = 0;
+    }
+    return ar
+  }
+
+  protected btnClick(data: any) {
+    if (data < 1) return;
+      if (data != this.currentPage) {
+        this.currentPage = data
+      }if(this.limitNum!==this.limit&&this.limitNum!==null){//每页显示条数改变
+        let pages=Math.ceil(this.totalcount / this.limitNum);
+        if(data>pages){
+          data=pages;
         }
-        console.log(arr[this.currentPage])
-        if(arr[this.currentPage]!==undefined){
-          arr[this.currentPage].classList.add("page-current");
-          arr[this.currentPage-1].classList.remove("page-current");
-          this.jumpPage(this.currentPage + 1)
-        }
-       
-      } 
-    }
-
-    protected  showPrevPage() {
-      this.currentPage = this.currentPage - this.pagegroup > 0 ? this.currentPage - this.pagegroup : 1
-    }
-
-    protected showNextPage() {
-      this.currentPage = this.currentPage + this.pagegroup < this.totalPage ? this.currentPage + this.pagegroup : this.totalPage
-      console.log(222)
-    }
-    
-    protected goPage (jumpPageNumber:number) {
-      console.log(jumpPageNumber)
-      if(Number(jumpPageNumber) <= 0){
-        jumpPageNumber = 1
-      }if(Number(jumpPageNumber) >= this.totalPage){
-        jumpPageNumber = this.totalPage
+        this.$emit('update:limit', this.limitNum);
       }
-      this.jumpPage(Number(jumpPageNumber))
-    }
+      this.$emit('update:currentPage', data);
+      this.$emit('change-handle', data)
     
-    protected jumpPage (pageNumber:number) {
-      console.log(pageNumber,this.currentPage)
-      var arr = document.getElementsByClassName("page-item");
-      if (this.currentPage !== pageNumber) { 
-        arr[pageNumber+1].classList.add("page-current");
-        arr[this.currentPage+1].classList.remove("page-current");
-         //点击的页码不是当前页码
-        this.currentPage = pageNumber
-      //父组件通过change方法来接受当前的页码
-      Number(this.currentSize)==pageNumber
-      if(this.currentPage>1){
-        arr[1].classList.remove("disabled")
-      }
-     
-      this.$emit('jumpPage', {currentPage: this.currentPage, currentSize: this.currentSize})
-      }
-    }
-    
-    protected input(e:HTMLInputEvent){
-      this.jumpPageNumber=e.target.value as any
-    }
+  }
 
-    protected render() :JSX.Element{
-        return(
-            <div class="page-helper">
-              {this.showPageHelper ? ( <div class="page-list">
-              <span>共{this.totalPage }页 / { this.totalcount }条</span>
-              <span class="page-item" onClick={e=>{this.jumpPage(1)}}>首页</span>
-              <span class={`page-item ${this.currentPage === 1? 'disabled':""}`}   onClick={this.prevPage}>上一页</span>
-              {this.showPrevMore ? ( <span class="page-ellipsis"  onClick={this.showPrevPage}>...</span>):(<span></span>)}
-              {this.groupList.map((val,index)=>{
-                  return(<span  class={`page-item page-common ${this.currentPage==val?'page-current':""}`} key={index} onClick={e=>{this.jumpPage(val)}}>{val}</span>)
-              })}
-              {this.showNextMore ? (<span class="page-ellipsis"  onClick={this.showNextPage}>...</span>):(<span></span>)}
-              <span  class={`page-item page-common }`}>{this.totalcount/this.totalPage}</span>
-              <span class={`page-item ${this.currentPage === this.totalPage?'disabled':"" }`}  onClick={this.nextPage}>下一页</span>
-              <span class="page-item" onClick={e=>{this.jumpPage(this.totalPage)}}>末页</span>
-              <select class="page-select" onChange={e=>{this.jumpPage(this.currentPage)}} value={this.currentSize}>
-                {this.pagesize.map((val,index)=>{
-                    return(<option  key={index} value={val}>{ val }条/页</option>)
-                })}
-              </select>
-              <span class="ml20">跳至</span>
-              <span class="page-jump-to"><input type="type" value={this.jumpPageNumber} onInput={e=>{this.input(e as any)}} /></span>
-              <span>页</span>
-              <span class="page-item jump-go-btn" onClick={e=>{this.goPage(this.jumpPageNumber)}}>GO</span>
-              </div>):(<div></div>)}   
-            </div >
-        )
+
+  protected nextPage(data: number) {
+    console.log(this.totalPage)
+    console.log(this.totalcount, this.limit)
+    if (this.currentPage >= this.totalPage) return;
+    this.btnClick(this.currentPage + 1);
+  }
+
+  protected prvePage(data: number) {
+    if (this.currentPage <= 1) return;
+    this.btnClick(this.currentPage - 1);
+  }
+
+  protected setButtonClass(isNext: any) {
+    if (isNext) {
+      return this.currentPage >= this.totalPage ? "page-button-disabled" : ""
     }
+    else {
+      return this.currentPage <= 1 ? "page-button-disabled" : ""
+    }
+  }
+
+  protected input(e:HTMLInputEvent){
+    this.jumpPageNumber=e.target.value as any
+    this.limitNum=e.target.value as any
+  }
+
+  protected  initLimitNums() {
+    this.limitNum=this.limit;
+    if (!this.limitNums.includes(this.limit)) {
+      this.limitNums.push(this.limit);
+      this.limitNums.sort(function (a, b) {
+        return a-b;
+      });
+    }
+  }
+
+  protected created(){
+    this.initLimitNums();
+  }
+
+  protected goPage (jumpPageNumber:number) {
+    if(Number(jumpPageNumber) <= 0){
+      jumpPageNumber = 1
+    }if(Number(jumpPageNumber) >= this.totalPage){
+      jumpPageNumber = this.totalPage
+    }
+    this.btnClick(Number(jumpPageNumber))
+  }
+
+  protected render(): JSX.Element {
+    return (
+      <div class="page-bar">
+        <ul>
+          {this.showtotal?( <li><a style="border:none">共{this.totalcount}条记录  第{this.currentPage}/{this.totalPage}页</a></li>):null}
+          <li><a class={this.setButtonClass(0)} onClick={e => { this.prvePage(this.currentPage) }}>上一页</a></li>
+          {this.indexs.map((val, index) => {
+            // console.log(this.indexs)
+            return (<li class={{ "active": this.currentPage === val }} key={index}>
+              <a onClick={e => { this.btnClick(val) }}>{val < 1 ? "..." : val}</a>
+            </li>
+            )
+          })}
+          <li><a class={this.setButtonClass(1)} onClick={e => { this.nextPage(this.currentPage) }}>下一页</a></li>
+          {this.showsize?( <li style="position: relative;width: 74px;">
+          <select  value={this.limitNum} onInput={e=>{this.input(e as any)}} onChange={e=>{this.btnClick(this.currentPage)}}>
+            {this.limitNums.map((val,index)=>{
+              return( <option value={val}>{val}条/页</option>)  
+            })}
+          </select>
+          </li>):null}
+          {this.showelevator?(<li><a style="border:none">跳至</a></li> ):null}
+          {this.showelevator?(<li><a style="border:none"><input type="number" style="width:60px;height:20px" value={this.jumpPageNumber} onInput={e=>{this.input(e as any)}} /></a></li>):null}
+          {this.showelevator?(<li><a style="border:none">页</a></li>):null}
+          {this.showelevator?(<li><a class="page-item jump-go-btn" onClick={e=>{this.goPage(this.jumpPageNumber)}}>GO</a></li>):null}        
+        </ul>
+      </div>
+
+    )
+  }
 
 }
